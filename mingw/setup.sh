fa8c355b80159fb032e084663bda36dbc1e464cb
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-01-07 23:14:40 +0200

    update

--- mingw/setup.sh
+++ mingw/setup.sh
@@ -9,7 +9,7 @@ echo "export CARGO_HOME=\$MINGW_PREFIX/.cargo" >> $PROFILE_PATH/rust.sh
 echo "export RUSTUP_HOME=\$MINGW_PREFIX/.rustup" >> $PROFILE_PATH/rust.sh
 
 # VSCode
-echo "export PATH=\$PATH:/c/Users/User/AppData/Local/Programs/Microsoft VS Code/bin" > $PROFILE_PATH/code.sh
+echo "export PATH=\"\$PATH:/c/Users/User/AppData/Local/Programs/Microsoft VS Code/bin\"" > $PROFILE_PATH/code.sh
 
 # Finish
 echo "Please execute"
